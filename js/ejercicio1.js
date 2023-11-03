8dd65223e06796183c59b19d6910d72bcb12dd8d
luchy0 <lucia.arroyo@cecar.edu.co>
2023-11-03 10:45:00 -0500

    Ejercicios Terminados: Api paises

---
+++ js/ejercicio1.js
@@ -0,0 +1,44 @@
+// Función para cargar datos de la API
+async function fetchData() {
+    try {
+        const response = await fetch("https://restcountries.com/v3.1/independent?status=true");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al cargar datos de la API:", error);
+        return [];
+    }
+}
+
+// Función para llenar la tabla con datos filtrados
+async function fillTable() {
+    const data = await fetchData();
+    const tableBody = document.querySelector("#countryTable tbody");
+
+    data.forEach(country => {
+        if (country.population > 100000) {
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <td>${country.name.common}</td>
+                <td>
+                    <ul>
+                        ${country.currencies ? Object.values(country.currencies).map(currency => `<li>${currency.name}</li>`).join("") : "N/A"}
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><img src="${country.flags.svg}" alt="${country.name.common}" width="50"></li>
+                    </ul> 
+                </td>
+                <td>${country.population}</td>
+                <td>
+                    <ul>
+                        ${country.capital.map(capital => `<li>${capital}</li>`).join("")}
+                    </ul>
+                </td>
+            `;
+            tableBody.appendChild(row);
+            console.log(country.name.common);
+        }
+    });
+}
